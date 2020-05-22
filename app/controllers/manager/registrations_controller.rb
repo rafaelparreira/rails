ff2f40b98db7e3ba9faaa34000c48330b2e98134
rafaelparreira <rafaeltp3@gmail.com>
2020-05-22 19:44:47 -0300

    init

--- app/controllers/manager/registrations_controller.rb
+++ app/controllers/manager/registrations_controller.rb
@@ -48,9 +48,9 @@ class Manager::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
